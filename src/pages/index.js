af78a0f4ccaaacb1a4f34e6307bdadcbee07a616
Rellyson Douglas <rellysondouglas2015@gmail.com>
2021-09-11 18:21:44 -0300

    changed file location

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Airbnb</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+    </div>
+  )
+}
