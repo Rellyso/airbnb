d090d76fe63d8e2ca86c4d61655500b7b68cdc5c
Rellyson Douglas <rellysondouglas2015@gmail.com>
2021-09-13 08:24:22 -0300

    init of Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+
+export function Header() {
+  return (
+    <header>
+      <div>
+        <Image
+          src="https://links.papareact.com/qd3"
+        />
+      </div>
+
+      <div></div>
+
+      <div></div>
+
+    </header>
+  )
+}
